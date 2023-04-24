f0ae208bb0cb3c347ad93386a389f651740ae9ba
Jan Jakub Zábranský <zabransky@SPSEJECNANET.CZ>
2023-04-24 07:55:29 +0200

    first commit

---
+++ 24.4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _24._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server srv = new Server(65525);
+        }
+    }
+}
---
+++ 24.4/Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._4
+{
+    public class Server
+    {
+        private TcpListener myServer;
+        private bool isRunning;
+
+        public Server(int port)
+        {
+            myServer = new TcpListener(System.Net.IPAddress.Any, port);
+            myServer.Start();
+            isRunning = true;
+            ServerLoop();
+        }
+
+        private void ServerLoop()
+        {
+            Console.WriteLine("Server byl spusten");
+            while (isRunning)
+            {
+                TcpClient client = myServer.AcceptTcpClient();
+                ClientLoop(client);
+            }
+        }
+
+        private void ClientLoop(TcpClient client)
+        {
+            StreamReader reader = new StreamReader(client.GetStream(), Encoding.UTF8);
+            StreamWriter writer = new StreamWriter(client.GetStream(), Encoding.UTF8);
+
+            writer.WriteLine("Byl jsi pripojen");
+            writer.Flush();
+            bool clientConnect = true;
+            string? data = null;
+            string? dataRecive = null;
+            while (clientConnect)
+            {
+                data = reader.ReadLine();
+                data = data.ToLower();
+                if (data == "end")
+                {
+                    clientConnect = false;
+                }
+
+                dataRecive = data + " prijato";
+                writer.WriteLine(dataRecive);
+                writer.Flush();
+            }
+            writer.WriteLine("Byl jsi odpojen");
+            writer.Flush();
+        }
+    }
+}
